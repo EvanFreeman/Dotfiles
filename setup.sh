a2159008ccb2dd445f0501188e59e083a9a7918c
Evan Freeman <efreeman@cainc.com>
2016-12-09 12:24:22 -0500

    Updates to plugins and configs
    Updates to support neovim
    Updates to setup scripts
    Updates to vimrc to speed up ctrlp

--- setup.sh
+++ setup.sh
@@ -2,5 +2,8 @@
 ln -s Dotfiles/bash/.bash_profile ~/.bash_profile
 ln -s Dotfiles/vim-files/vim ~/.vim
 ln -s Dotfiles/vim-files/vim/vimrc ~/.vimrc
+ln -s Dotfiles/gitmessage ~/.gitmessage
+ln -s Dotfiles/gitmessage ~/.gitmessage
+
 
 ln -s Dotfiles/sublime-text-files/Preferences.sublime-settings  ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
